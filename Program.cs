bd0e24721e339c6b8c0067f1a79a0311908d72ea
“00Amit0” <amit836961@gmail.com>
2022-06-10 01:58:09 +0530

    Counting words in a sentence

--- Program.cs
+++ Program.cs
@@ -5,6 +5,25 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome , let's count words in a string");
+            string paragraph = "To be or not to be";
+            Console.WriteLine(paragraph);
+            CountWords(paragraph);
+            Console.ReadLine();
+        }
+        /// Counts the words.
+        public static void CountWords(string paragraph)
+        {
+            MyMapNode<string, int> myMapNode = new MyMapNode<string, int>(6);
+            string[] words = paragraph.Split(' ');
+            foreach (string word in words)
+            {
+                if (myMapNode.Exists(word))
+                    myMapNode.Add(word.ToLower(), myMapNode.Get(word) + 1);
+                else
+                    myMapNode.Add(word.ToLower(), 1);
+            }
+            Console.WriteLine("Display after Add operation");
+            myMapNode.Display();
         }
     }
 }
